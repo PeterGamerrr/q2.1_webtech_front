be77c1d62d9da1f7ada1d3d0bebdad0b6f104de7
Peter_Gamerrr <497155@student.saxion.nl>
2021-10-12 10:31:38 +0200

    getting the data from the backend

--- auction/src/components/itemList/ItemList.svelte
+++ auction/src/components/itemList/ItemList.svelte
@@ -2,15 +2,33 @@
     import SearchBar from "../SearchBar.svelte";
     import ItemListContent from "./ItemListContent.svelte";
     import ItemListPager from "./ItemListPager.svelte";
+    import {onMount} from "svelte"
     import {filteredItems} from "../../stores/filteredItems";
 
     export let items = [];
     export let itemType = "beer";
     export let listPageSize = 1;
 
+    onMount(async () => {
+        items = await getData();
+        $filteredItems = items;
+    })
+
+    async function getData() {
+        const res = await fetch("http://localhost:3000/api/auctions");
+        console.log(res)
+        if (res.ok) {
+            const json = await res.json();
+            console.log(json)
+            return json;
+        } else {
+            const text = await res.text();
+            throw new Error(text);
+        }
+    }
+
     let currListPage = 1;
 
-    $filteredItems = items;
 
     const updateCurrListPage = (event) => {
         currListPage = event.detail.text;
@@ -22,11 +40,18 @@
 </script>
 
 
-<SearchBar input={items} on:searchBarOutput={searchBarOutput}/>
-<div class="content">
-    <ItemListContent {currListPage} {listPageSize} {itemType}/>
-</div>
-<ItemListPager {listPageSize} on:updateCurrListPage={updateCurrListPage}/>
+{#await items}
+    <p>...Waiting</p>
+{:then items}
+    {console.log(items)}
+    <SearchBar input={items} on:searchBarOutput={searchBarOutput}/>
+    <div class="content">
+        <ItemListContent {currListPage} {listPageSize} {itemType}/>
+    </div>
+    <ItemListPager {listPageSize} on:updateCurrListPage={updateCurrListPage}/>
+{:catch error}
+    <p>{error.message}</p>
+{/await}
 
 
 <style>
